0a797dbbb91056a563a406ee8e9d76c89bb0b86e
ztevoz <ztevoz77@gmail.com>
2021-09-11 20:55:05 +0200

    Charts et error page

--- src/main/java/net/secudev/crudy/controller/ProduitController.java
+++ src/main/java/net/secudev/crudy/controller/ProduitController.java
@@ -1,8 +1,12 @@
 package net.secudev.crudy.controller;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,16 +28,36 @@ public class ProduitController extends AController {
 
 	@GetMapping("produit/populate")
 	public String populate() {
-		populator.initProduit(100);
+		populator.initProduit(300);
 		return "redirect:/produit/liste";
+	}
+
+	@GetMapping("produit/destroy")
+	public String destroy() {
+		produits.deleteAll();
+		return "redirect:/produit/liste";
+	}
+
+	@GetMapping("produit/stats")
+	public String stats(Model model) {
+		
+		Map<String, Integer> tab = new LinkedHashMap<>();
+
+		for (Produit p : produits.findAll(Sort.by(Sort.Direction.DESC, "stock"))) {			
+			tab.put(p.getLibelle(), p.getStock());
+		}
+		
+		System.out.println(tab);
+		model.addAttribute("tab", tab);
+
+		return "produit/stats";
 	}
 
 	@GetMapping("produit/liste")
 	public String listProduits(Model model, @RequestParam(name = "page", defaultValue = "0") int page,
 			@RequestParam(name = "size", defaultValue = "10") int size) {
 
-		if(produits.count()==0) populator.initProduit(100);
-		Page<Produit> pageProduits = produits.findAll(PageRequest.of(page, size, Direction.DESC, "stock","prixAchat"));
+		Page<Produit> pageProduits = produits.findAll(PageRequest.of(page, size, Direction.DESC, "stock", "prixAchat"));
 		model.addAttribute("pageProduits", pageProduits);
 		model.addAttribute("pageNumber", page);
 		model.addAttribute("size", size);
@@ -41,7 +65,7 @@ public class ProduitController extends AController {
 		model.addAttribute("totalPages", pageProduits.getTotalPages());
 		model.addAttribute("pages", new int[pageProduits.getTotalPages()]);
 
-		//nombre de bouttons de pages -1
+		// nombre de bouttons de pages -1
 		model.addAttribute("buttonPages", 9);
 
 		return "/produit/liste";
